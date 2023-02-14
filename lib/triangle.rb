9d2d38acb813861dd6938bea1df6346c6170711e
vivianne <viviannjoroge91@gmail.com>
2023-02-14 10:30:00 +0300

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
-  # write code here
+  class TriangleError < StandardError
+  end
+
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3]
+  end
+
+  def kind
+    if @sides.any? { |side| side <= 0 }
+      raise TriangleError, "Triangle sides must be positive"
+    elsif @sides.max >= @sides.reduce(:+) - @sides.max
+      raise TriangleError, "Invalid triangle sides"
+    elsif @sides.uniq.length == 1
+      :equilateral
+    elsif @sides.uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
